1c06af279c29ffb369a9ae20689527612301c80f
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-08-12 15:43:40 +0200

    setup routing and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from 'react-router';
+import { motion } from 'motion/react';
+
+function Home() {
+  return (
+    <motion.h1
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      Accueil
+    </motion.h1>
+  );
+}
+
+function Project() {
+  return <h1>Projet</h1>;
+}
+
 function App() {
   return (
-    <main>
-      <h1>Portfolio</h1>
-    </main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projets/:slug" element={<Project />} />
+    </Routes>
   );
 }
 

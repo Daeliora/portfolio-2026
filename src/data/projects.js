c707cfd46ae97daa415e5d904f560797f282e4c9
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-08-12 15:51:22 +0200

    create project data model

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+const projects = [
+    
+  {
+    title: 'Kasa',
+    slug: 'kasa',
+    shortDescription: "Développement d'une application de location immobilière avec React.",
+    technologies: ['React', 'React Router', 'CSS'],
+    image: '/images/kasa-cover.webp',
+    links: {
+      live: '',
+      github: '',
+    },
+    context: `Projet réalisé dans le cadre de ma formation de développeuse web. L'objectif était de développer une application de location immobilière
+à partir d'une maquette fournie.`,
+    objectives: [
+      'Développer une interface avec React',
+      'Créer des composants réutilisables',
+      'Mettre en place la navigation entre les différentes pages',
+      'Respecter la maquette et les contraintes responsive',
+    ],
+    technicalStack: [
+      {
+        name: 'React',
+        description:
+          "Bibliothèque JavaScript utilisée pour construire l'interface.",
+      },
+      {
+        name: 'React Router',
+        description:
+          "Gestion de la navigation et des différentes routes de l'application.",
+      },
+      {
+        name: 'CSS',
+        description:
+          "Mise en forme de l'interface et adaptation aux différentes tailles d'écran.",
+      },
+    ],
+    skills: [
+      'Architecture React',
+      'Création de composants réutilisables',
+      'Gestion de la navigation',
+      'Responsive design',
+      'Organisation d’un projet front-end',
+    ],
+    results: "Une application fonctionnelle reproduisant l'interface demandée et permettant de naviguer entre les différentes pages.",
+    improvements: "Une évolution possible serait d'ajouter davantage de fonctionnalités et de rendre certaines parties de l'application plus dynamiques.",
+  },
+
+];
+
+export default projects;
